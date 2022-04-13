8702c1c91dbf62768375dc65b25a8312c38d6969
amaburi02 <sherrynguyenhawaii@gmail.com>
2022-04-12 22:49:42 -0500

    learned how to check for user id

--- main.py
+++ main.py
@@ -23,9 +23,14 @@ async def on_ready():
     #)
     #''')
     print('{0.user} launching...'.format(bot))
+    return await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a cassette tape"))
 
 @bot.command()
 async def hello(ctx):
-    await ctx.reply('Hello!')
+    authorID = ctx.message.author.id
+    if authorID == 600444714856218634:
+        await ctx.reply('Hello Sherry!')
+    else:
+        await ctx.reply('Hello!')
 
 bot.run(os.environ['TOKEN'])
